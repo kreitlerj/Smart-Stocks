3806ea3ed9fc512cb173624e4e96dec8856f1849
kreitlerj <kreitlerj11@yahoo.com>
2019-04-02 23:44:21 -0500

    update models, add functions.py, update app.py

--- app/app.py
+++ app/app.py
@@ -9,11 +9,9 @@ import alpha_vantage
 from alpha_vantage.timeseries import TimeSeries
 from flask import Flask, jsonify, render_template
 from sklearn.preprocessing import MinMaxScaler
-# from keras.models import Sequential
-# from keras.layers import Dense, Dropout, LSTM
 from keras.models import load_model
 from numpy import array
-# from functions import one_year_data
+from functions import build_input
 import tensorflow as tf
 global graph,model
 graph = tf.get_default_graph()
@@ -44,8 +42,7 @@ def stock_chart(stock):
     url = "https://www.alphavantage.co/query?"
     query_url = f"{url}function=&symbol={stock}&outputsize=full&apikey=JH6O3VJXUFU3WLSZ"
 
-# @app.route("/ticker")
-# def names():
+
 
 @app.route("/data/<stock>")
 def data(stock):
@@ -64,10 +61,8 @@ def data(stock):
 
     df_filtered = df_transposed[yesterday : past_year] 
 
+    #format the input dataframe
     df_filtered.rename(columns={'1. open': 'open', '4. close':'close', '3. low':'low', '2. high':'high', '5. volume':'volume'}, inplace=True)
-    
-    
-    
     df_filtered.open = pd.to_numeric(df_filtered.open, errors='coerce')
     df_filtered.close = pd.to_numeric(df_filtered.close, errors='coerce')
     df_filtered.low = pd.to_numeric(df_filtered.low, errors='coerce')
@@ -75,103 +70,44 @@ def data(stock):
     df_filtered.volume = pd.to_numeric(df_filtered.volume, errors='coerce')
     df_filtered.index = pd.to_datetime(df_filtered.index)
     df_filtered = df_filtered.sort_index(ascending=True)
-    valid = df_filtered   
-    df_filtered = df_filtered.drop('open', axis=1)
-    df_filtered = df_filtered[["close", "low", "high", "volume"]]
+    df_filtered = df_filtered[["open", "close", "low", "high", "volume"]]
     
-    #sixty day model
-    dataset = df_filtered.values
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    scaled_data = scaler.fit_transform(dataset)
-    X_test, y_test = [], []
-    for i in range(60,len(dataset)):
-        X_test.append(scaled_data[i-60:i,0:4])
-        y_test.append(scaled_data[i,0])
-    X_test, y_test = np.array(X_test), np.array(y_test)
-    
+    #build the inputs for the models
+    x_sixty, current_sixty, x_thirty, current_thirty, x_ten, current_ten, y_scalar = build_input(df_filtered)
+
+    #run the models
     with graph.as_default():
-        closing_price = sixty_day_model.predict(X_test)
+        closing_price_sixty = sixty_day_model.predict(x_sixty)
+        current_sixty_pred = sixty_day_model.predict(current_sixty)
+        closing_price_thirty = thirty_day_model.predict(x_thirty)
+        current_thirty_pred = thirty_day_model.predict(current_thirty)
+        closing_price_ten = ten_day_model.predict(x_ten)
+        current_ten_pred = ten_day_model.predict(current_ten)
     
-    final = []
-    for price in closing_price:
-        final.append(np.pad(price, (0, 3), 'constant'))
-    final_price = scaler.inverse_transform(final)
-    close = []
-    for price in final_price:
-        close.append(price[0])
+    #scalar the outputs
+    closing_price_sixty = y_scalar.inverse_transform(closing_price_sixty)
+    current_sixty_pred = y_scalar.inverse_transform(current_sixty_pred)
+    closing_price_thirty = y_scalar.inverse_transform(closing_price_thirty)
+    current_thirty_pred = y_scalar.inverse_transform(current_thirty_pred)
+    closing_price_ten = y_scalar.inverse_transform(closing_price_ten)
+    current_ten_pred = y_scalar.inverse_transform(current_ten_pred)
     
-    valid_sixty_day = df_filtered[60:] 
-    valid_sixty_day["predictions"] = close
-    valid_sixty_day = valid_sixty_day["predictions"]
-    
-    #thirty day model 
-    dataset = df_filtered.values
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    scaled_data = scaler.fit_transform(dataset)
-    X_test, y_test = [], []
-    for i in range(30,len(dataset)):
-        X_test.append(scaled_data[i-30:i,0:4])
-        y_test.append(scaled_data[i,0])
-    X_test, y_test = np.array(X_test), np.array(y_test)
-    
-    with graph.as_default():
-        closing_price = thirty_day_model.predict(X_test)
-    
-    final = []
-    for price in closing_price:
-        final.append(np.pad(price, (0, 3), 'constant'))
-    final_price = scaler.inverse_transform(final)
-    close = []
-    for price in final_price:
-        close.append(price[0])
-    
-    valid_thirty_day = df_filtered[30:] 
-    valid_thirty_day["predictions"] = close
-    valid_thirty_day = valid_thirty_day["predictions"]
-    
-    #ten day model
-    dataset = df_filtered.values
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    scaled_data = scaler.fit_transform(dataset)
-    X_test, y_test = [], []
-    for i in range(10,len(dataset)):
-        X_test.append(scaled_data[i-10:i,0:4])
-        y_test.append(scaled_data[i,0])
-    X_test, y_test = np.array(X_test), np.array(y_test)
-    
-    with graph.as_default():
-        closing_price = ten_day_model.predict(X_test)
-    
-    final = []
-    for price in closing_price:
-        final.append(np.pad(price, (0, 3), 'constant'))
-    final_price = scaler.inverse_transform(final)
-    close = []
-    for price in final_price:
-        close.append(price[0])
-    
-    valid_ten_day = df_filtered[10:]  
-    valid_ten_day["predictions"] = close
-    valid_ten_day = valid_ten_day["predictions"]
-    
-    #output to json
-    valid_sixty_day = valid_sixty_day[-90:]
-    valid_thirty_day = valid_thirty_day[-90:]
-    valid_ten_day = valid_ten_day[-90:]
-    valid = valid[-90:]
+    #build dataframe to compare predictions
+    valid_sixty_day = closing_price_sixty[-90:]
+    valid_thirty_day = closing_price_thirty[-90:]
+    valid_ten_day = closing_price_ten[-90:]
+    valid = df_filtered[-90:]
     valid['sixty'] = valid_sixty_day
     valid['thirty'] = valid_thirty_day
     valid['ten'] = valid_ten_day
+
+    #set error variables
     sixty_error = (valid['close'] - valid['sixty']).mean()
     thirty_error = (valid['close'] - valid['thirty']).mean()
     ten_error = (valid['close'] - valid['ten']).mean()
 
-    
+    #output to json
     valid = valid.to_json(orient='index')
-    # response = {}
-    # response['valid'] = valid
-    # response['predictions'] = {'sixty': sixty_day_pred, 'thirty': thirty_day_pred, 'ten': ten_day_pred}
-    # response['error'] = {'sixty': sixty_error, 'thirty': thirty_error, 'ten': ten_error}
     
     return valid
 
---
+++ app/functions.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pandas as pd
+import random
+from sklearn.preprocessing import MinMaxScaler
+
+def build_input(dataframe):
+    x_df = dataframe
+    y_df = dataframe[['close']]
+    x_scaler = MinMaxScaler(feature_range=(0, 1))
+    y_scaler = MinMaxScaler(feature_range=(0, 1))
+    x_pred_dataset = x_df.values
+    y_pred_dataset = y_df.values
+    x_scaled_data = x_scaler.fit_transform(x_pred_dataset)
+    y_scaled_data = y_scaler.fit_transform(y_pred_dataset)
+    x_pred_sixty, y_pred_sixty = [], []
+    x_pred_thirty, y_pred_thirty = [], []
+    x_pred_ten, y_pred_ten = [], []
+    for i in range(60, len(dataframe)):
+        x_pred_sixty.append(x_scaled_data[i-60:i, :])
+        y_pred_sixty.append(y_scaled_data[i, :])
+    x_pred_sixty, y_pred_sixty = np.array(x_pred_sixty), np.array(y_pred_sixty)
+
+    for i in range(30, len(dataframe)):
+        x_pred_thirty.append(x_scaled_data[i-30:i, :])
+        y_pred_thirty.append(y_scaled_data[i, :])
+    x_pred_thirty, y_pred_thirty = np.array(x_pred_thirty), np.array(y_pred_thirty)
+
+    for i in range(10, len(dataframe)):
+        x_pred_ten.append(x_scaled_data[i-10:i, :])
+        y_pred_ten.append(y_scaled_data[i, :])
+    x_pred_ten, y_pred_ten = np.array(x_pred_ten), np.array(y_pred_ten)
+
+    current_sixty, current_thirty, current_ten = [], [], []
+    for i in range(0, 60):
+        current_sixty.append(x_scaled_data[i-60])
+    x_current_sixty = np.array(current_sixty)
+    x_current_sixty = np.reshape(x_current_sixty, (1,) + x_current_sixty.shape)
+
+    for i in range(0, 30):
+        current_thirty.append(x_scaled_data[i-30])
+    x_current_thirty = np.array(current_thirty)
+    x_current_thirty = np.reshape(x_current_thirty, (1,) + x_current_thirty.shape)
+
+    for i in range(0, 10):
+        current_ten.append(x_scaled_data[i-10])
+    x_current_ten = np.array(current_ten)
+    x_current_ten = np.reshape(x_current_ten, (1,) + x_current_ten.shape)
+
+    return(x_pred_sixty, x_current_sixty, x_pred_thirty, x_current_thirty, x_pred_ten, x_current_ten, y_scaler)
