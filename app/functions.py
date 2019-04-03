915574de7db7e22f8ed9c1d1b8aeb8abca5197c0
Jake Kreitler <43191311+kreitlerj@users.noreply.github.com>
2019-04-03 08:10:45 -0500

    Update functions.py

--- app/functions.py
+++ app/functions.py
@@ -47,3 +47,31 @@ def build_input(dataframe):
     x_current_ten = np.reshape(x_current_ten, (1,) + x_current_ten.shape)
 
     return(x_pred_sixty, x_current_sixty, x_pred_thirty, x_current_thirty, x_pred_ten, x_current_ten, y_scaler)
+
+def build_current_input(dataframe):
+    x_df = dataframe
+    y_df = dataframe[['close']]
+    x_scaler = MinMaxScaler(feature_range=(0, 1))
+    y_scaler = MinMaxScaler(feature_range=(0, 1))
+    x_pred_dataset = x_df.values
+    y_pred_dataset = y_df.values
+    x_scaled_data = x_scaler.fit_transform(x_pred_dataset)
+    y_scaled_data = y_scaler.fit_transform(y_pred_dataset)
+    
+    current_sixty, current_thirty, current_ten = [], [], []
+    for i in range(0, 60):
+        current_sixty.append(x_scaled_data[i-60])
+    x_current_sixty = np.array(current_sixty)
+    x_current_sixty = np.reshape(x_current_sixty, (1,) + x_current_sixty.shape)
+
+    for i in range(0, 30):
+        current_thirty.append(x_scaled_data[i-30])
+    x_current_thirty = np.array(current_thirty)
+    x_current_thirty = np.reshape(x_current_thirty, (1,) + x_current_thirty.shape)
+
+    for i in range(0, 10):
+        current_ten.append(x_scaled_data[i-10])
+    x_current_ten = np.array(current_ten)
+    x_current_ten = np.reshape(x_current_ten, (1,) + x_current_ten.shape)
+    
+    return(x_current_sixty, x_current_thirty, x_current_ten, y_scaler)
